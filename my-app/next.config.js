4a39a8bc88a2b1b9ee905d7c86597987f4e7418d
Karthikeyan <get2karthim@gmail.com>
2025-06-17 20:29:45 +0530

    fix deployment issue

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
-  }
+  },
+  output: 'standalone',
+  poweredByHeader: false,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/_next' : '',
+  trailingSlash: false
 }
 
 module.exports = nextConfig
