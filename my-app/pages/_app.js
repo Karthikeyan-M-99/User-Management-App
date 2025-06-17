bf4a3db519a4205f1ee497eb8c9ef9a31c52a59f
Karthikeyan <get2karthim@gmail.com>
2025-06-17 09:14:05 +0530

    fix for deployment error

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ConfigProvider } from 'antd';
-import 'antd/dist/reset.css';
 import store from '../src/store';
 import MainLayout from '../src/components/layout/MainLayout';
 import AuthGuard from '../src/gaurds/AuthGuard';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+  const isServer = typeof window === 'undefined';
+
+  if (isServer) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <Head>
--- my-app/src/httpClient.ts
+++ my-app/src/httpClient.ts
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
+const isServer = typeof window === 'undefined';
+
 const httpClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+  baseURL: 'https://users-management-api.vercel.app',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
   }
 });
 
 httpClient.interceptors.request.use(
   (config) => {
-    if (typeof window !== 'undefined') {
+    if (!isServer) {
       const token = localStorage.getItem('token');
       if (token && token !== 'undefined') {
         config.headers.Authorization = `Bearer ${token}`;
